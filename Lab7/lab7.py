7a4fbf0f16c89eace0b7d86b93a6d9aa7aa520b5
davide-perli <163366267+davide-perli@users.noreply.github.com>
2025-12-01 16:52:13 +0200

    ex2, 3

--- Lab7/lab7.py
+++ Lab7/lab7.py
@@ -2,10 +2,6 @@ from scipy import ndimage, datasets
 import numpy as np
 import matplotlib.pyplot as plt
 
-# X = datasets.face(gray=True)
-# plt.imshow(X, cmap=plt.cm.gray)
-# plt.show()
-
 # Exercitiul 1
 # a)
 
@@ -69,7 +65,7 @@ Y = np.zeros((N, N), dtype=complex)
 Y[0][5] = Y[0][N-5] = 1
 
 X = np.fft.irfft2(Y)
-freq_db = 20*np.log10(abs(Y))
+freq_db = 20*np.log10(abs(Y) + 1e-10)
 
 fig, ax = plt.subplots(1, 2, figsize=(12, 12))
 
@@ -91,7 +87,7 @@ Y = np.zeros((N, N), dtype=complex)
 Y[5][0] = Y[-5][0] = 1
 
 X = np.fft.irfft2(Y)
-freq_db = 20*np.log10(abs(Y))
+freq_db = 20*np.log10(abs(Y) + 1e-10)
 
 fig, ax = plt.subplots(1, 2, figsize=(12, 12))
 
@@ -113,7 +109,7 @@ Y = np.zeros((N, N), dtype=complex)
 Y[5][5] = Y[-5][-5] = 1
 
 X = np.fft.irfft2(Y)
-freq_db = 20*np.log10(abs(Y))
+freq_db = 20*np.log10(abs(Y) + 1e-10)
 
 fig, ax = plt.subplots(1, 2, figsize=(12, 12))
 
@@ -125,4 +121,69 @@ plt.colorbar(spectru_X, ax=ax[0])
 plt.colorbar(spectru_frec_Y, ax=ax[1])
 plt.tight_layout()
 plt.savefig("./Lab7/ex1_e.pdf", format="pdf")
+plt.show()
+
+# Exercitiul 2
+
+freq_cutoff = 40
+X = datasets.face(gray=True)
+# Normalizare val pix intre 0 si 1
+X = (X - X.min()) / (X.max() - X.min())
+Y = np.fft.fft2(X)
+freq_db = 20*np.log10(abs(Y) + 1e-10)
+Y_cutoff = Y.copy()
+Y_cutoff[freq_db < freq_cutoff] = 0 # In exemplul din lab am eliminat magnitudini mari, aici elimin magnitudini mici pentru a nu se duce informatia, ci doar calitatea pozei vine redusa
+X_cutoff = np.fft.ifft2(Y_cutoff).real
+
+snr_db = 10*np.log10(np.mean(X ** 2) / np.mean((X - X_cutoff) ** 2))
+print(f"SNR obtinut: {snr_db:.2f} dB") 
+fig, ax = plt.subplots(1, 2, figsize=(12, 12))
+
+ax[0].imshow(X, cmap='gray')
+ax[1].imshow(X_cutoff, cmap='gray')
+ax[0].set_title("Imagine originala")
+ax[1].set_title(f"Imagine comprimata\nSNR = {snr_db:.2f} dB")
+plt.tight_layout()
+plt.savefig("./Lab7/ex2.pdf", format="pdf")
+plt.show()
+
+# Exerctitiul 3 
+
+pixel_noise = 200
+X = datasets.face(gray=True)
+noise = np.random.randint(-pixel_noise, high=pixel_noise+1, size=X.shape)
+X_noisy = X + noise
+snr_db_inainte = 10*np.log10(np.mean(X ** 2) / np.mean((noise) ** 2))
+print("SNR inainte: ", snr_db_inainte)
+Y_noisy = np.fft.fft2(X_noisy)
+Y_shifted = np.fft.fftshift(Y_noisy)
+
+rows, cols = X.shape
+middle_row, middle_col = rows // 2, cols // 2
+radius = 80  # mai mic => filtrarea este mai puternica
+mask = np.zeros_like(X)
+Y, X_grid = np.ogrid[:rows, :cols]
+mask = (Y - middle_row) ** 2 + (X_grid - middle_col) ** 2 <= radius ** 2
+
+Y_filtered = Y_shifted * mask
+X_filtered = np.real(np.fft.ifft2(np.fft.ifftshift(Y_filtered)))
+
+snr_db_dupa = 10 * np.log10(np.mean(X**2) / np.mean((X - X_filtered)**2))
+print("SNR dupa filtrare:", snr_db_dupa)
+
+fig, ax = plt.subplots(3, 1, figsize=(10, 18))  # 3 rânduri, 1 coloană
+ax[0].imshow(X, cmap='gray')
+ax[0].set_title("Imagine originala", fontsize=12)
+ax[0].axis('off')
+
+ax[1].imshow(X_noisy, cmap='gray')
+ax[1].set_title(f"Imagine zgomotoasa\nnoise = {pixel_noise}, SNR inainte = {snr_db_inainte:.2f} dB", fontsize=12)
+ax[1].axis('off')
+
+ax[2].imshow(X_filtered, cmap='gray')
+ax[2].set_title(f"Imagine filtrata\nSNR dupa = {snr_db_dupa:.2f} dB", fontsize=12)
+ax[2].axis('off')
+
+plt.subplots_adjust(hspace=1)
+plt.savefig("./Lab7/ex3.pdf", format="pdf")
 plt.show()
